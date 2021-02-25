b4c31cdba7254cb570464c1312c04742f1626e2e
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-25 12:56:55 +0300

    add share button and general style

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, Button, Share } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
-import Button from "@ant-design/react-native/lib/button";
 import WingBlank from "@ant-design/react-native/lib/wing-blank";
 import ShakeEventExpo from "../config/ShakeEventExpo";
 import WhiteSpace from "@ant-design/react-native/lib/white-space";
@@ -72,6 +71,25 @@ function HomeScreen(props) {
     });
   }, [isPressed, api]);
 
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: `_❝ ${quote.quote} ❞_\n \n*-${quote.author}*`,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   if (!fontsLoaded) {
     return <Text>...</Text>;
   } else {
@@ -80,46 +98,57 @@ function HomeScreen(props) {
         <Text style={styles.appName}>Quote Machine</Text>
         <WingBlank size="lg">
           <Button
-            type="primary"
+            color="#C4B92C"
+            title="Press to generate a quote!"
             onPress={() => {
               setIsPressed(isPressed + 1);
             }}
-            style={styles.generalText}
-          >
-            Press to generate a quote!
-          </Button>
+            style={[styles.generalText, styles.button]}
+          />
 
           <WhiteSpace size="xl" />
 
-          <Text style={styles.generalText}>or shake your phone!</Text>
+          <Text style={styles.generalText}>or shake your phone&#128075; </Text>
         </WingBlank>
         <WingBlank size="lg">
           <WhiteSpace size="xl" />
           {api ? (
-            <View style={styles.quoteBox}>
-              <Text style={styles.text}>
-                <Text style={styles.quotationMark}>&#x201C;</Text>
-                {quote.quote}
-                <Text style={styles.quotationMark}>&#x201D;</Text>
-              </Text>
-              <Text style={styles.header}>-{quote.author}</Text>
+            <View>
+              <View style={styles.quoteBox}>
+                <Text style={styles.text}>
+                  <Text style={styles.quotationMark}>&#x201C;</Text>
+                  {quote.quote}
+                  <Text style={styles.quotationMark}>&#x201D;</Text>
+                </Text>
+                <Text style={styles.header}>-{quote.author}</Text>
+              </View>
+              <Button
+                color="#C4B92C"
+                style={styles.button}
+                disabled={quote.quote ? false : true}
+                onPress={onShare}
+                title="Share &#128640;"
+              />
             </View>
           ) : (
-            <Text style={styles.text}>Please choose a quote!</Text>
+            <Text style={styles.generalText}>
+              Please choose an option &#10549;
+            </Text>
           )}
         </WingBlank>
 
-        <RNPickerSelect
-          style={styles.picker}
-          onValueChange={(value) => setApi(value)}
-          items={[
-            { label: "Kanye West", value: "kanye" },
-            { label: "Ron Swanson", value: "ron" },
-            { label: "Taylor Swift", value: "swift" },
-            { label: "Breaking Bad", value: "breakingBad" },
-            { label: "Random Quotes", value: "randomQuotes" },
-          ]}
-        />
+        <View style={styles.picker}>
+          <RNPickerSelect
+            onValueChange={(value) => setApi(value)}
+            items={[
+              { label: "Kanye West", value: "kanye" },
+              { label: "Ron Swanson", value: "ron" },
+              { label: "Taylor Swift", value: "swift" },
+              { label: "Breaking Bad", value: "breakingBad" },
+              { label: "Random Quotes", value: "randomQuotes" },
+            ]}
+          />
+        </View>
       </View>
     );
   }
@@ -128,7 +157,7 @@ function HomeScreen(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "space-around",
   },
@@ -137,22 +166,23 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     fontFamily: "LobsterTwo_400Regular_Italic",
     fontSize: 16,
+    color: "white",
   },
   button: {
-    padding: 5,
-    backgroundColor: "red",
+    padding: 15,
   },
   picker: {
-    width: "100%",
-    backgroundColor: "red",
-    color: "red",
-    paddingHorizontal: 10,
+    backgroundColor: "#C4B92C",
+    padding: 20,
+    borderRadius: 25,
+    width: Platform.OS === "android" ? "100%" : null,
   },
   header: {
     fontSize: 20,
     fontWeight: "bold",
     alignSelf: "flex-end",
-    fontFamily: "LobsterTwo_400Regular_Italic",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    color: "white",
   },
   quotationMark: {
     fontSize: 25,
@@ -162,15 +192,19 @@ const styles = StyleSheet.create({
     borderColor: "grey",
     borderWidth: 5,
     padding: 10,
+    marginBottom: 20,
   },
   generalText: {
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     fontSize: 20,
+    color: "white",
+    alignSelf: "center",
   },
   appName: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: "bold",
     fontFamily: "Orbitron_400Regular",
+    color: "white",
   },
 });
 
